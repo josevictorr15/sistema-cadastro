3bbf78a3bf1a3f1ab0087363dbf0cedf803168ae
Jose <victorsantanaa2011@hotmail.com>
2024-01-30 15:38:26 -0300

    Inicializando o Cadastro do Usuário

--- frontend/crud/src/components/user/UserCrud.jsx
+++ frontend/crud/src/components/user/UserCrud.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Component } from 'react'
 import Main from '../template/Main.jsx'
+import axios from 'axios'
+
+const baseUrl = "http://localhost:5173/users"
+const initialState = {
+    user: "", email: "", list: []
+}
 
 const headerProps = {
     icon: "users",
@@ -9,6 +15,29 @@ const headerProps = {
 }
 
 export default class UserCrud extends Component {
+    state = { ...initialState }
+
+    clear() {
+        this.setState({user: initialState.user})
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? "put" : "post"
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(response => {
+                const list = this.getUpdateList(response.data)
+                this.setState({ user: initialState.user, list })
+            })
+    }
+
+    getUpdateList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps}> 
