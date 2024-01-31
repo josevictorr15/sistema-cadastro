32c394fbdd4c1ab950987bfd75b9672873e199b2
Jose <victorsantanaa2011@hotmail.com>
2024-01-31 09:53:30 -0300

    Renderizando o formulário

--- frontend/crud/src/components/user/UserCrud.jsx
+++ frontend/crud/src/components/user/UserCrud.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 import { Component } from 'react'
 import Main from '../template/Main.jsx'
 import axios from 'axios'
 
-const baseUrl = "http://localhost:5173/users"
+const baseUrl = "http://localhost:5174/users"
+
 const initialState = {
-    user: "", email: "", list: []
+    user: { name: "", email: "" },
+    list: []
 }
 
 const headerProps = {
@@ -30,6 +33,7 @@ export default class UserCrud extends Component {
                 const list = this.getUpdateList(response.data)
                 this.setState({ user: initialState.user, list })
             })
+            .catch(error => console.log(error))
     }
 
     getUpdateList(user) {
@@ -38,10 +42,47 @@ export default class UserCrud extends Component {
         return list
     }
 
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({user})
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className='form-control' name='name' value={this.state.user.name} onChange={e => this.updateField(e)} placeholder='Digite o nome...'/>
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className='form-control' name='email' value={this.state.user.email} onChange={e => this.updateField(e)} placeholder='Digite o email...'/>
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary" onClick={e => this.save(e)}>Salvar</button>
+                        <button className='btn btn-secondary ml-2' onClick={e => this.clear(e)}>Cancelar</button>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main {...headerProps}> 
-                Cadastro de Usuário
+                {this.renderForm()}
             </Main>
         )
     }
